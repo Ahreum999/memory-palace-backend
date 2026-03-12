c7309706fdeb0b1129b2c1e19639ade0ecbb6fd3
Ahreum Lee <ahreumlee@Ahreums-MacBook-Pro.local>
2026-03-11 21:37:05 -0400

    remove hardcoded passwords use env var only

--- api/db.js
+++ api/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:YqPFZxFQtNzOMREvzqLuNInEHSjhORbY@maglev.proxy.rlwy.net:44365/railway',
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
